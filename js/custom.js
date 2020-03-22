0f394f3930a514f5b442df421c4e501854703308
abayomi <53374703+unbeatable-abayomi@users.noreply.github.com>
2020-03-22 17:00:36 +0100

    addedLinks

--- js/custom.js
+++ js/custom.js
@@ -4,3 +4,18 @@ function gotoURL (select_element) {
 }
 
 
+// function prime(n)
+// { 
+//     if (n < 2)
+//     {
+//         return false
+//     }
+//     for (var i = 2; i < n; i++)
+//     {
+//        if (n % i == 0)
+//         return false;
+//     }
+//     return true
+// }
+
+// console.log(prime(1))
